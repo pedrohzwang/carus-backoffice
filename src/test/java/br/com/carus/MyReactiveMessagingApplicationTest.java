2991c13d6a19cc6d8a4bb19caedf0fb16891bb93
Pedro Zwang <pedro.zwang@paytrack.com.br>
2023-05-28 19:17:32 -0300

    first commit

---
+++ src/test/java/br/com/carus/MyReactiveMessagingApplicationTest.java
@@ -0,0 +1,24 @@
+package br.com.carus;
+
+import br.com.carus.kafka.consumers.MyReactiveMessagingApplication;
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.junit.jupiter.api.Test;
+
+import jakarta.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+class MyReactiveMessagingApplicationTest {
+
+    @Inject
+    MyReactiveMessagingApplication application;
+
+    @Test
+    void test() {
+        assertEquals("HELLO", application.toUpperCase(Message.of("Hello")).getPayload());
+        assertEquals("BONJOUR", application.toUpperCase(Message.of("bonjour")).getPayload());
+    }
+}
---
+++ src/test/java/br/com/carus/StatusResourceIT.java
@@ -0,0 +1,8 @@
+package br.com.carus;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class StatusResourceIT extends StatusResourceTest {
+    // Execute the same tests but in packaged mode.
+}
